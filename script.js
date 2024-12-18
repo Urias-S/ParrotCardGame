84781419443e6d7803b61b4e72fcdb763c8bf3d4
Vinicius <viniciusmoura761@gmail.com>
2024-12-17 21:38:20 -0300

    Lógica de pontuação e fim de jogo

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let jogadas = 0;
+let cartasViradas = 0;
 function qtdeCartasJogo() {
   let qtde = Number(prompt("Com quantas cartas você quer jogar?"));
   let numeroInvalido = (qtde < 4 || qtde > 14) || (qtde % 2 === 1);
@@ -39,8 +41,52 @@ function iniciarJogo(){
 }
 iniciarJogo();
 function virarCarta(elemento){
+  if (cartasViradas >2 ) {
+    return;
+  }
   const frente = elemento.querySelector(".frente");
-  const verso = elemento.querySelector(".verso")
-  frente.classList.add("viraFrente");
-  verso.classList.add("viraVerso");
+  const verso = elemento.querySelector(".verso") 
+  if (!elemento.classList.contains("ativa") && !elemento.classList.contains("temp") && cartasViradas < 2) {
+    elemento.classList.add("temp");
+    frente.classList.add("viraFrente");
+    verso.classList.add("viraVerso");
+    cartasViradas++;
+    jogadas++;
+  }
+  console.log(jogadas);
+  setTimeout(conferePar, 500); 
+}
+function conferePar(){
+  let temporarias = document.querySelectorAll(".temp");
+  if (temporarias.length > 1) {
+    if (temporarias[0].innerHTML == temporarias[1].innerHTML && (!temporarias[0].classList.contains("ativa") && !temporarias[1].classList.contains("ativa"))){
+      temporarias[0].classList.add("ativa");
+      temporarias[1].classList.add("ativa");
+      temporarias[0].classList.remove("temp");
+      temporarias[1].classList.remove("temp");
+      temporarias = [];
+      cartasViradas = 0;
+      
+    } else if (!temporarias[0].classList.contains("ativa") && !temporarias[1].classList.contains("ativa")) setTimeout(function desviraCarta() {
+      {  
+        temporarias[0].classList.remove("temp");
+        temporarias[1].classList.remove("temp");
+        temporarias[0].querySelector(".frente").classList.remove("viraFrente");
+        temporarias[0].querySelector(".verso").classList.remove("viraVerso");
+        temporarias[1].querySelector(".frente").classList.remove("viraFrente");
+        temporarias[1].querySelector(".verso").classList.remove("viraVerso");
+        temporarias = []; 
+        cartasViradas = 0;
+      }
+    },1000)
+  }
+  fimJogo();
+}
+function fimJogo(){
+  const cartasAtivas = document.querySelectorAll(".carta.ativa");
+  const totalDeCartas = document.querySelectorAll(".carta").length;
+  if (cartasAtivas.length === totalDeCartas) {
+    alert(`Você ganhou em ${jogadas} jogadas!`);
+  }
+
 }
